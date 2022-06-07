4eb60f9834e411bce2223f49d96ac1263cbd7fb9
Giulio Bernardi <giulioccbernardi@gmail.com>
2022-06-06 22:18:13 -0300

    Criei DAO modelo com tudo pronto, penos queries personalizadas

--- src/main/java/br/com/fiap/JkControl/dao/impl/ClasseModeloDAO.java
+++ src/main/java/br/com/fiap/JkControl/dao/impl/ClasseModeloDAO.java
@@ -5,8 +5,19 @@ import br.com.fiap.JkControl.entity.ClasseModelo;
 
 public class ClasseModeloDAO extends HibernateGenericDAO<ClasseModelo, Long> {
 
+    private static ClasseModeloDAO instance = null;
+
+    public static ClasseModeloDAO getInstance() {
+        if (instance == null){
+            instance = new ClasseModeloDAO();
+        }
+        return instance;
+    }
+
 
     public ClasseModeloDAO() {
         super(ClasseModelo.class);
     }
+
+    //TODO queries necessárias
 }
