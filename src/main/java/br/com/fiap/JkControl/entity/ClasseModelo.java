b01056e907daf9e558eb2b93e3cce33b31b7a83d
Giulio Bernardi <giulioccbernardi@gmail.com>
2022-06-06 22:14:30 -0300

    Coloquei principais tags jpa para agilizar o processo da criação da classe

--- src/main/java/br/com/fiap/JkControl/entity/ClasseModelo.java
+++ src/main/java/br/com/fiap/JkControl/entity/ClasseModelo.java
@@ -1,5 +1,23 @@
 package br.com.fiap.JkControl.entity;
 
+import javax.persistence.*;
+
+@Entity
+@SequenceGenerator(name = "", sequenceName = "SQ_TB_", allocationSize = 1)
 public class ClasseModelo {
+
+    @Id
+    @GeneratedValue(generator = "", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
     //TODO alterar atributos de acordo com o que o professor passar
+
+    //Copiar e colar esse atributo com essa tag para facilitar
+    @Column(name = "")
+    private String TROCARNOME;
+
+
+
+
+    //TODO método toString personalizado
 }
